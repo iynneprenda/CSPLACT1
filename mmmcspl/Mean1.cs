1452046de2602df49121557114c0af1d7255e8a8
iynneprenda <145856181+iynneprenda@users.noreply.github.com>
2024-04-03 20:11:40 +0800

    Add files via upload

---
+++ mmmcspl/Mean1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mmmcspl
+{
+    public class Mean1
+    {
+        public double ComputeMeanPeriod(List<double> grades)
+        {
+            double total = 0;
+            foreach (var grade in grades)
+            {
+                total += grade;
+            }
+            return total / grades.Count;
+        }
+
+        public (double prelimMean, double midtermMean, double finalsMean) ComputeMeanPeriod(List<student> students)
+        {
+            List<double> prelimGrades = new List<double>();
+            List<double> midtermGrades = new List<double>();
+            List<double> finalsGrades = new List<double>();
+
+            foreach (var student in students)
+            {
+                prelimGrades.Add(student.Prelim);
+                midtermGrades.Add(student.Midterm);
+                finalsGrades.Add(student.Finals);
+            }
+
+            double prelimMean = ComputeMeanPeriod(prelimGrades);
+            double midtermMean = ComputeMeanPeriod(midtermGrades);
+            double finalsMean = ComputeMeanPeriod(finalsGrades);
+
+            return (prelimMean, midtermMean, finalsMean);
+        }
+        public List<double> ComputeFinalAverages(List<student> students)
+        {
+            List<double> finalAverages = new List<double>();
+
+            foreach (var student in students)
+            {
+                double finalAverage = (student.Prelim + student.Midterm + student.Finals) / 3.0;
+                finalAverages.Add(finalAverage);
+            }
+
+            return finalAverages;
+        }
+    }
+}
+
+
+
+
+
+           
